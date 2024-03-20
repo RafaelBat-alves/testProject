e4bb272893511e62f29a6e51c05537d2db8f7f8d
RafaelBat-alves <152424427+RafaelBat-alves@users.noreply.github.com>
2024-03-20 16:25:29 -0300

    lambda

--- Main.java
+++ Main.java
@@ -20,7 +20,8 @@ public class Main {
 //Collect: permite coletar os elementos da stream em uma coleção ou em outro tipo de dado
 //O que é flatMap?
 //O método flatMap() primeiro mapeia cada elemento usando uma função de mapeamento e, em seguida, nivela o resultado em um novo array 
-//Uma stream é uma sequência de elementos que pode ser processada em paralelo ou em série. Ela pode ser criada a partir de uma coleção, um array, um arquivo, entre outros.        
+//Uma stream é uma sequência de elementos que pode ser processada em paralelo ou em série. Ela pode ser criada a partir de uma coleção, um array, um arquivo, entre outros. 
+//flatMap() --> dentro da minha lista eu vou ter outra lista e vou agrutina tudo em um        
 
     }
 }
